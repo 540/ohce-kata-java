85a0ec454ddb5b132ab2c3940ed44899b9a0cc08
Gorka <gorka@540deg.com>
2020-02-03 22:52:00 +0100

    Improve example code

--- src/main/java/com/deg540/tdd/Calculator.java
+++ src/main/java/com/deg540/tdd/Calculator.java
@@ -2,7 +2,7 @@ package com.deg540.tdd;
 
 class Calculator {
     public int add(int number1, int number2) {
-        return number1 + number2;
+        return 0;
     }
 
     public int multiply(int number1, int number2) {
--- src/test/java/com/deg540/tdd/CalculatorShould.java
+++ src/test/java/com/deg540/tdd/CalculatorShould.java
@@ -24,7 +24,5 @@ public class CalculatorShould {
 
         assertThat(result, is(2));
     }
-
-
 }
 
